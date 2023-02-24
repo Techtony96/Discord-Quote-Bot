c8f16ee0f76324be3d5b5aab4706f787c4ffbe3d
Tony Pappas <techtony96@gmail.com>
2023-02-23 17:03:55 -0700

    Format version command

--- src/main/java/net/ajpappas/discordquotebot/command/VersionCommand.java
+++ src/main/java/net/ajpappas/discordquotebot/command/VersionCommand.java
@@ -1,15 +1,24 @@
 package net.ajpappas.discordquotebot.command;
 
+import discord4j.common.util.TimestampFormat;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.time.Instant;
 
 @Component
 public class VersionCommand implements SlashCommand {
 
+
     @Value("${git.commit.id.describe-short}")
     private String version;
+
+    @Value("${git.commit.time}")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
+    private Instant commitTime;
 
     @Override
     public String getName() {
@@ -18,6 +27,10 @@ public class VersionCommand implements SlashCommand {
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
-        return event.reply().withEphemeral(true).withContent(version);
+        return event.reply().withEphemeral(true).withContent(String.format("Version: `%s`\nDate: %s", version, getFormattedCommitTime()));
+    }
+
+    private String getFormattedCommitTime() {
+        return String.format("%s (%s)", TimestampFormat.SHORT_DATE_TIME.format(commitTime), TimestampFormat.RELATIVE_TIME.format(commitTime));
     }
 }
